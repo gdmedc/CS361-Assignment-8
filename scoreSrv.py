519def286aa0ee3cafe609e86831ec63276bd541
gdmed <59933843+gdmed@users.noreply.github.com>
2023-05-07 13:45:55 -0700

    Updated scoreSrv function

--- scoreSrv.py
+++ scoreSrv.py
@@ -4,23 +4,40 @@ import time
 while True:
     time.sleep(1)
     # Bonds, Cash, TBills, BAccnts, Stocks, RlEstate, MFunds, Options, Futures, Collectibles 
-    weights = [2, 1, 1, 1, 4, 1, 3, 10, 10, 8]
+    weights = {"Bonds": 2, 
+               "Cash": 1, 
+               "TreasuryBills": 1, 
+               "BankAccounts": 1,
+               "Stocks": 4,
+               "RealEstate": 1,
+               "MutualFunds": 3,
+               "Options": 10,
+               "Futures": 10,
+               "Collectibles": 8}
     message = open("message.txt","r+")
     # Erase contents then write random number to file
     if message.read() == "run":
-        assets = numpy.loadtxt("data.txt", usecols = 0, dtype = float)
+        # Read asset types from column 1
+        assetType = []
+        data = open("data.txt","r+")
+        lines = data.readlines()
+        for line in lines:
+            assetType.append(line.split()[0])
+        size = len(assetType)
+        data.close()
+        
         amounts = numpy.loadtxt("data.txt", usecols = 1, dtype = float)
         # Multiply assets by risk factor and sum; Also sum amounts
-        asSum = 0
+        riskSum = 0
         amSum = 0
-        for i in range(10):
-            assets[i] *= weights[i]
-            asSum += assets[i]
+        for i in range(size):          
+            riskSum += amounts[i] * weights[assetType[i]]
             amSum += amounts[i]
             
         if amSum == 0: amSum = 1         
         # Calculate score and return message
-        score = asSum//amSum
+        score = riskSum//amSum
+        
         if score < 1: 
             x = 'You are broke!'
         elif score == 1: 
